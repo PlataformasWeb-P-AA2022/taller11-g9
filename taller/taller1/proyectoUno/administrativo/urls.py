4872decff290cd6561e6040fa4575ae637ce030a
RommelOjeda <rommelojeda7@gmail.com>
2022-06-28 15:08:52 -0500

    avance

--- taller/taller1/proyectoUno/administrativo/urls.py
+++ taller/taller1/proyectoUno/administrativo/urls.py
@@ -8,23 +8,4 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
-        # Urls Edificio
-        path('edificio/<int:id>', views.obtenerEdificio, 
-            name='obtenerEdificio'),
-        path('agregar/edificio', views.agregarEdificio, 
-            name='agregarEdificio'),
-        path('editarEdificio/<int:id>', views.editarEdificio, 
-            name='editarEdificio'),
-        path('eliminar/edificio/<int:id>', views.eliminarEdificio, 
-            name='eliminarEdificio'),
-        # Urls Departamento
-        path('agregar/departamento', views.agregarDepartamento, 
-            name='agregarDepartamento'),
-        path('editar/departamento/<int:id>', views.editarDepartamento, 
-            name='editarDepartamento'),
-        path('eliminar/departamento/<int:id>', views.eliminarDepartamento, 
-            name='eliminarDepartamento'),
-        # Urls Departamento
-        path('agregar/departamento/edificio/<int:id>', views.agregarDepartamentoEdificio, 
-            name='agregarDepartamentoEdificio'),
  ]
